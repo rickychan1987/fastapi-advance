fe63975dc3ddd2a519b999e302d5fd3cae9aed0c
rickychan1987 <khaivanla1987@gmail.com>
2022-04-01 00:40:08 +0700

    Upload Profile

--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # models.Base.metadata.create_all(bind=engine)
 
@@ -12,3 +23,8 @@ app.include_router(post.router, prefix="/posts", tags=['Post'])
 app.include_router(user.router, prefix="/users", tags=['User'])
 app.include_router(vote.router, prefix="/vote", tags=['Vote'])
 app.include_router(auth.router, tags=['Authentication'])
+
+
+@app.get('/')
+async def root():
+    return {"message": "Welcome to Ricky API"}
