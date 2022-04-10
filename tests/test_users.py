de8684f6970f4a52080451ab583507362a3ca25c
rickychan1987 <khaivanla1987@gmail.com>
2022-04-10 11:40:21 +0700

    Setup Testing Database

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,7 @@
+from hashlib import new
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
@@ -9,3 +11,11 @@ def test_root():
     print(res.json().get('message'))
     assert res.json().get('message') == 'Welcome to Ricky API'
     assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post(
+        "/users/", json={"email": "hello123@gmail.com", "password": "1234"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
