3f1474cdec9b0ba3671f3dcb9c91081ac316a233
rickychan1987 <khaivanla1987@gmail.com>
2022-04-28 12:26:27 +0700

    Testing - Create post test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,21 +1,46 @@
-from hashlib import new
-from fastapi.testclient import TestClient
-from app.main import app
+import pytest
+from jose import jwt
 from app import schemas
 
-client = TestClient(app)
+from app.config import settings
 
 
-def test_root():
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert res.json().get('message') == 'Welcome to Ricky API'
-    assert res.status_code == 200
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Welcome to Ricky API'
+#     assert res.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "hello123@gmail.com", "password": "1234"})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', "1234", 403),
+    ('ricky@gmail.com', 'wrongpass', 403),
+    ('wrongemail@gmail.com', 'wrongpass', 403),
+    (None, '1234', 422),
+    ('ricky@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == "Invalid Credentials"
