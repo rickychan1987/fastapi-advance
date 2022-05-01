cdc7eb755c32329afba3fbf7ac086504b935d3b6
rickychan1987 <62366969+rickychan1987@users.noreply.github.com>
2022-05-01 15:46:23 +0700

    Updated

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -19,13 +19,13 @@ def test_unauthorized_user_get_all_posts(client, test_posts):
     assert res.status_code == 401
 
 
-def test_unauthorized_user_get_one_posts(client, test_posts):
+def test_unauthorized_user_get_one_post(client, test_posts):
     res = client.get(f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
 
 
 def test_get_one_post_not_exist(authorized_client, test_posts):
-    res = authorized_client.get(f"/posts/8888")
+    res = authorized_client.get(f"/posts/88888")
     assert res.status_code == 404
 
 
@@ -37,12 +37,12 @@ def test_get_one_post(authorized_client, test_posts):
     assert post.Post.title == test_posts[0].title
 
 
-@pytest.mark.parametrize("title, content, published", {
+@pytest.mark.parametrize("title, content, published", [
     ("awesome new title", "awesome new content", True),
-    ("favorite pizza", "i love sanwich", False),
+    ("favorite pizza", "i love pepperoni", False),
     ("tallest skyscrapers", "wahoo", True),
-})
-def test_create_post(authorized_client, test_posts, test_user, title, content, published):
+])
+def test_create_post(authorized_client, test_user, test_posts, title, content, published):
     res = authorized_client.post(
         "/posts/", json={"title": title, "content": content, "published": published})
 
@@ -54,25 +54,25 @@ def test_create_post(authorized_client, test_posts, test_user, title, content, p
     assert created_post.owner_id == test_user['id']
 
 
-def test_create_post_default_published_true(authorized_client, test_posts, test_user):
+def test_create_post_default_published_true(authorized_client, test_user, test_posts):
     res = authorized_client.post(
-        "/posts/", json={"title": "abcd", "content": "abc1234"})
+        "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
 
     created_post = schemas.Post(**res.json())
     assert res.status_code == 201
-    assert created_post.title == "abcd"
-    assert created_post.content == "abc1234"
+    assert created_post.title == "arbitrary title"
+    assert created_post.content == "aasdfjasdf"
     assert created_post.published == True
     assert created_post.owner_id == test_user['id']
 
 
 def test_unauthorized_user_create_post(client, test_user, test_posts):
     res = client.post(
-        "/posts/", json={"title": "abcd", "content": "abc1234"})
+        "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
     assert res.status_code == 401
 
 
-def test_unauthorized_user_delete_post(client, test_user, test_posts):
+def test_unauthorized_user_delete_Post(client, test_user, test_posts):
     res = client.delete(
         f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
@@ -81,19 +81,21 @@ def test_unauthorized_user_delete_post(client, test_user, test_posts):
 def test_delete_post_success(authorized_client, test_user, test_posts):
     res = authorized_client.delete(
         f"/posts/{test_posts[0].id}")
+
     assert res.status_code == 204
 
 
 def test_delete_post_non_exist(authorized_client, test_user, test_posts):
     res = authorized_client.delete(
-        f"/posts/888888")
+        f"/posts/8000000")
+
     assert res.status_code == 404
 
 
 def test_delete_other_user_post(authorized_client, test_user, test_posts):
     res = authorized_client.delete(
         f"/posts/{test_posts[3].id}")
-    assert res.status_code == 204
+    assert res.status_code == 403
 
 
 def test_update_post(authorized_client, test_user, test_posts):
@@ -118,7 +120,7 @@ def test_update_other_user_post(authorized_client, test_user, test_user2, test_p
 
     }
     res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
-    assert res.status_code == 200
+    assert res.status_code == 403
 
 
 def test_unauthorized_user_update_post(client, test_user, test_posts):
